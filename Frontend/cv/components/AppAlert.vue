d59c950bd0edf7edf0d717056ee294cbfdf130eb
GERALD1983 <opstyle1@hotmail.fr>
2023-07-27 14:55:18 +0200

    ajout de d 2 pages test

---
+++ Frontend/cv/components/AppAlert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    je suis un composant
+
+    <h1>Welcome to the composant</h1>
+  </div>
+</template>
+<script setup lang="ts">
+let dormir = "dr";
+console.log(dormir);
+</script>
+<style></style>
---
+++ Frontend/cv/pages/page2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    je suis la page 2
+    <NuxtWelcome />
+    <AppAlert />
+  </div>
+</template>
+<script setup lang="ts">
+let manger: string | number = "st";
+manger = 2;
+console.log(manger);
+</script>
+<style>
+body {
+  background-color: antiquewhite;
+}
+</style>
