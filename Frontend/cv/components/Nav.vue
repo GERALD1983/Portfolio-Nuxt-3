7930ede022afc91b3665ef152e9d74eeb6879c89
GERALD1983 <opstyle1@hotmail.fr>
2023-07-28 16:42:06 +0200

    ajout composant nav et logo vers typescript et nuxt3

---
+++ Frontend/cv/components/Nav.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    id="nav"
+    class="navSize mobileNavSize d-flex justify-content-between align-items-center"
+  >
+    <div @click="toTop" class="ombreNav">
+      <Logo />
+    </div>
+
+    <div class="mobileNavProjet col-lg-5 px-0 d-flex justify-content-around">
+      <div
+        @click="toProject"
+        class="mobileNavLink30 mobileNavLink70 px-0 ombreNav col-md-4 col-lg-5 bgPrimary bordureLogo d-flex align-items-center justify-content-center"
+      >
+        <h4 class="lienSouris my-1">Projets</h4>
+      </div>
+      <div
+        @click="toFoot"
+        class="mobileNavLink30 mobileNavLink70 px-0 ombreNav col-md-4 col-lg-5 bgPrimary bordureLogo d-flex align-items-center justify-content-center"
+      >
+        <h4 class="lienSouris my-1">Contact</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import jump from "jump.js";
+onMounted(() => {
+  var navbar = document.getElementById("nav") as HTMLElement;
+  var header = document.getElementById("backMaPage") as HTMLElement;
+  var visible = header.offsetHeight;
+  console.log(visible);
+  window.addEventListener("scroll", () => {
+    let scrollValue = window.scrollY;
+    console.log(scrollValue);
+    if (scrollValue > visible) {
+      navbar.classList.add("navFixed");
+    } else {
+      navbar.classList.remove("navFixed");
+    }
+  });
+});
+
+function toTop() {
+  // window.scrollTo(0, 400);
+  jump("#backMaPage", {
+    duration: 1000,
+    callback: () => console.log("Saut terminé!"),
+  });
+}
+function toFoot() {
+  // window.scrollTo(0, 400);
+  jump(".ancreForm", {
+    duration: 1000,
+    callback: () => console.log("Saut terminé!"),
+  });
+}
+const toProject = () => {
+  // window.scrollTo(0, 400);
+  jump("#titreProjet", {
+    duration: 1000,
+    callback: () => console.log("Saut terminé!"),
+  });
+};
+</script>
+
+<style>
+.navNoOpac {
+  opacity: 1 !important;
+}
+.navFixed {
+  position: fixed;
+  z-index: 9;
+  top: 0;
+  opacity: 0.5;
+}
+.navSize {
+  height: 90px;
+
+  background: #9e9d9d;
+  width: 100%;
+}
+.ombreNav:hover {
+  /* box-shadow: 0 0 0 0 #748928ff; */
+  box-shadow: 0 0 0 0 rgb(23, 255, 236);
+  animation: pulse 1.3s infinite;
+  /* background: #9e9d9d !important;
+    color: white !important;*/
+}
+.bordureLogo {
+  border-radius: 0.5em;
+
+  background: gray;
+  color: white;
+  border: 0.1px solid #ffdea4;
+  /*border: 1mm ridge#ffdea4;*/
+}
+</style>
--- Frontend/cv/pages/index.vue
+++ Frontend/cv/pages/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
+    <div id="backMaPage">salut</div>
     je suis la page 1
+    <Nav />
 
-    <AppAlert />
+    <div id="titreProjet">salut</div>
+    <div class="ancreForm">salut</div>
   </div>
 </template>
 <script setup lang="ts">
 let manger = "st";
 console.log(manger);
 </script>
-<style></style>
+<style>
+#backMaPage {
+  margin-bottom: 400px;
+  height: 500px;
+  background-color: aqua;
+}
+#titreProjet {
+  margin-bottom: 400px;
+  height: 500px;
+  background-color: red;
+}
+.ancreForm {
+  margin-top: 400px;
+  height: 500px;
+  background-color: yellowgreen;
+}
+</style>
