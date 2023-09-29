9d1ff99be3ca6dc5572c5153d44464f9b3a0d910
GERALD1983 <opstyle1@hotmail.fr>
2023-09-29 12:02:41 +0200

    reglage animation carte version 1

--- Frontend/cv/components/VueCartePresentation.vue
+++ Frontend/cv/components/VueCartePresentation.vue
@@ -19,31 +19,29 @@
         class="box"
         srcImg="../assets/1710299.svg"
         titre="Scratcher"
-        sousTitre1="Vous vous demandez si cette expérience peut vous apportez de la valeur !"
-        sousTitre2="une expérience utilisateur fluide, et une
+        sousTitre1="Peut vous apportez de la valeur !"
+        sousTitre2="Expérience utilisateur fluide,
         gestion efficace des données."
-        sousTitre3="Créé pour l'efficacité, afin que vous
-        puissiez vous concentrer sur la création."
+        sousTitre3="Créé pour l'efficacité, concentrer sur la création."
       />
-      <!-- <Carte
-        srcImg="../assets/2128601.svg"
-        titre="Skiller"
-        sousTitre1="Vous vous demandez si cette expérience peut vous apportez de la valeur !"
-        sousTitre2="une expérience utilisateur fluide, et une
-        gestion efficace des données."
-        sousTitre3="Créé pour l'efficacité, afin que vous
-        puissiez vous concentrer sur la création."
-      />
-
       <Carte
+        class="box"
         srcImg="../assets/1710299.svg"
-        titre="Puncher"
-        sousTitre1="Vous vous demandez si cette expérience peut vous apportez de la valeur !"
-        sousTitre2="une expérience utilisateur fluide, et une
+        titre="Scratcher"
+        sousTitre1="Peut vous apportez de la valeur !"
+        sousTitre2="Expérience utilisateur fluide,
         gestion efficace des données."
-        sousTitre3="Créé pour l'efficacité, afin que vous
-        puissiez vous concentrer sur la création."
-      /> -->
+        sousTitre3="Créé pour l'efficacité, concentrer sur la création."
+      />
+      <Carte
+        class="box"
+        srcImg="../assets/1710299.svg"
+        titre="Scratcher"
+        sousTitre1="Peut vous apportez de la valeur !"
+        sousTitre2="Expérience utilisateur fluide,
+        gestion efficace des données."
+        sousTitre3="Créé pour l'efficacité, concentrer sur la création."
+      />
     </div>
   </div>
 </template>
@@ -53,79 +51,57 @@
   margin-left: 30px;
   margin-right: 30px;
 }
-html {
-  /* height: 100%; */
-  background: #223;
-  /* display: grid;
-  place-items: center; */
+/* html {
+  height: 100%; */
+/* background: #223; */
+/* display: grid;
+  place-items: center;
 }
+*/
 
 .box {
-  position: relative;
-  border: 2px solid rgb(89, 254, 0);
+  /* position: relative; */
+  border: 1px solid rgb(221, 221, 221);
   /*  */
-
-  border-radius: 20px;
-  -webkit-animation: bg-spin 3s linear infinite;
-  animation: bg-spin 3s linear infinite;
-  --border-size: 1px;
-  --border-angle: 0turn;
-  /* border: 0px solid rgba(255, 255, 255, 0) !important; */
-
-  background: conic-gradient(
-    from var(--border-angle),
-    transparent 20%,
-    rgb(1, 255, 52),
-    rgb(255, 255, 255) 50%,
-    rgb(6, 36, 231)
-  );
-  background-size: calc(100% - (var(--border-size) * 2))
-      calc(100% - (var(--border-size) * 2)),
-    cover;
-
-  background-position: center center;
-  background-repeat: no-repeat;
-}
-.box::after {
-  content: "";
-  inset: 0px;
-  position: absolute;
-  border: 2px solid rgb(255, 255, 255);
-  background-color: rgb(255, 255, 255);
 
   border-radius: 15px;
 
-  /* width: 60vmin;
-  height: 50vmin; */
+  background: white;
+
+  background-position: center center;
+  background-repeat: no-repeat;
+
+  /* border: 0px solid rgba(255, 255, 255, 0) !important; */
 }
-/* .box:hover {
+
+.box:hover {
   -webkit-animation: bg-spin 3s linear infinite;
   animation: bg-spin 3s linear infinite;
-  --border-size: 1px;
+
+  --border-size: 2px;
   --border-angle: 0turn;
+
   border: 1px solid rgba(255, 255, 255, 0) !important;
 
-  border-radius: 1em;
-  content: "";
-  inset: 2px;
+  border-radius: 15px;
 
-   width: 60vmin;
-  height: 50vmin; 
+  width: 60vmin;
+  height: 50vmin;
 
   background-image: conic-gradient(
       from var(--border-angle),
       rgb(255, 255, 255),
-      rgb(35, 35, 35) 50%,
+      rgb(255, 255, 255) 75%,
       rgb(255, 255, 255)
     ),
-    conic-gradient(from var(--border-angle), transparent 3%, #0afcf0, #00ffc3);
-  background-size: calc(100% - (var(--border-size) * 2))
-      calc(100% - (var(--border-size) * 2)),
+    conic-gradient(from var(--border-angle), transparent 3%, #0afcf0, #00ff7b);
+  background-size: calc(99% - (var(--border-size) * 2))
+      calc(99% - (var(--border-size) * 2)),
     cover;
 
   background-position: center center;
   background-repeat: no-repeat;
-} */
+}
 
 /************************/
 
