12d612429e535fe24a61dd75510aab8990376e53
GERALD1983 <opstyle1@hotmail.fr>
2023-10-03 19:39:12 +0200

    ajout du slider projet

---
+++ Frontend/cv/components/SliderProjet.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex-around">
+    <div class="sliders">
+      <div class="slides flex-around">
+        <div class="slide"><img src="../assets/RESTO370.jpg" alt="" /></div>
+        <div class="slide"><img src="../assets/RESTO370.jpg" alt="" /></div>
+        <div class="slide"><img src="../assets/RESTO370.jpg" alt="" /></div>
+        <div class="slide"><img src="../assets/RESTO370.jpg" alt="" /></div>
+        <div class="slide"><img src="../assets/RESTO370.jpg" alt="" /></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style>
+.sliders {
+  width: 100%;
+  margin: 30px;
+  overflow: hidden;
+}
+.slides {
+  width: calc(370px * 5);
+  animation: defile 20s infinite linear;
+  animation-play-state: running;
+}
+.slides:hover {
+  animation-play-state: paused;
+}
+.slide {
+  margin: 20px;
+  float: left;
+}
+@keyframes defile {
+  0% {
+    transform: translate(62%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
